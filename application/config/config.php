59705eff03811a3b3096369bd0bc0e5a52e35fc6
Haseeb04092002 <163792769+Haseeb04092002@users.noreply.github.com>
2026-02-09 10:20:14 +0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/rmc-web-ci';
+// $config['base_url'] = 'http://localhost/rmc-web';
+$config['base_url'] = 'https://softwares.itimium.com.pk/rmc-web';
 
 /*
 |--------------------------------------------------------------------------
